c125bacc407196d37f82416e9b2e533722349030
sharwinbobde <bsharwin@gmail.com>
2020-09-11 15:40:19 +0200

    added backend functions

---
+++ backend/firebase/functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+
+exports.addUserToFirestore = functions.auth.user().onCreate(user=>{
+
+    var doc = {
+        email: user.email,
+        uid: user.uid,
+    }
+
+    admin.firestore().collection('users').add(doc)
+    
+})
